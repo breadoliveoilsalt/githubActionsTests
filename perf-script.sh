e152313fcacee3737bdae0e15cf8283ad4b87a3d
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2023-05-25 17:12:27 -0400

    refactor script and add pipefail - works!

--- perf-script.sh
+++ perf-script.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-# set -Eeou pipefail
+set -eo pipefail
 
 ENV=$1
 TOKEN=$2
 URLS=""
+LOG_FILE="curl_log.tmp"
+BRANCH="lighthouseTesting"
+OK_STATUS_CODE_RESPONSE="204"
 
-if [[ ("$ENV" != "dev") && ("$ENV" != "qa") && ("$ENV" != "prod") ]]; then
-  echo 'ERROR: Must provide an argument for the environment: "dev", "qa", or "prod"'
-  exit 1
-fi
+function validate_args {
+  if [[ ("$ENV" != "dev") && ("$ENV" != "qa") && ("$ENV" != "prod") ]]; then
+    echo 'ERROR: Must provide an argument for the environment: "dev", "qa", or "prod"'
+    exit 1
+  fi
+}
 
 function generate_urls {
   while read -r line; do URLS+="$line"'\n'; done < pathnames.txt
 }
 
-generate_urls
-echo "URLS"
-echo "$URLS"
-
-# "urls": "https://www.breadoliveoilsalt.com/\nhttps://www.example.com/\n"
 function data_to_send_to_github {
   cat << EOF
 {
-  "ref": "lighthouseTesting",
+  "ref": "$BRANCH",
   "inputs": {
     "env": "$ENV",
     "urls": "$URLS"
@@ -32,28 +32,30 @@ function data_to_send_to_github {
 EOF
 }
 
-echo "data: $(data_to_send_to_github)"
+function trigger_github_action {
+  curl -iLSs \
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $TOKEN" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    "https://api.github.com/repos/breadoliveoilsalt/githubActionsTests/actions/workflows/lighthouse-test-2.yml/dispatches" \
+    -d "$(data_to_send_to_github)" \
+    | tee "$LOG_FILE"
+}
 
-LOG_FILE="curl_log.tmp"
+function check_trigger_succeded {
+  if grep -q "$OK_STATUS_CODE_RESPONSE" <(cat "$LOG_FILE" | head -1); then
+    echo "Data uploaded to Github"
+    rm "$LOG_FILE"
+    exit 0
+  else
+    echo "::error::perf-script.sh failed"
+    rm "$LOG_FILE"
+    exit 1
+  fi
+}
 
-# This works with workflow_dispatch
-curl -iLSs \
-  -X POST \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $TOKEN" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  "https://api.github.com/repos/breadoliveoilsalt/githubActionsTests/actions/workflows/lighthouse-test-2.yml/dispatches" \
-  -d "$(data_to_send_to_github)" \
-  | tee "$LOG_FILE"
-
-OK_STATUS_CODE_RESPONSE="204"
-
-if grep -q "$OK_STATUS_CODE_RESPONSE" <(cat "$LOG_FILE" | head -1); then
-  echo "Data uploaded to Github"
-  rm "$LOG_FILE"
-  exit 0
-else
-  echo "::error::perf-script.sh failed"
-  rm "$LOG_FILE"
-  exit 1
-fi
+validate_args
+generate_urls
+trigger_github_action
+check_trigger_succeded
