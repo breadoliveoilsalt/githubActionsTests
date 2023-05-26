71b0b866a9f51e72dc025c31e7d382f46cf4a6fa
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2023-05-26 11:13:59 -0400

    Clean up: exernalize token, fix set, etc.

--- perf-script.sh
+++ perf-script.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-set -eo pipefail
+set -euo pipefail
 
 ENV=$1
-TOKEN=$2
 BRANCH="main"
 DEV="dev"
 QA="qa"
@@ -53,7 +52,7 @@ function trigger_github_action {
   curl -iLSs \
     -X POST \
     -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer $TOKEN" \
+    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
     "https://api.github.com/repos/breadoliveoilsalt/githubActionsTests/actions/workflows/lighthouse-test.yml/dispatches" \
     -d "$(data_to_send_to_github)" \
